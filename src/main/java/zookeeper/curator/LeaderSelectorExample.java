f0f7e739c38b732def50fe9b3b0912b7de03b1dd
cjie888 <cjie888@163.com>
2016-03-14 18:17:50 +0800

    curator test

---
+++ src/main/java/zookeeper/curator/LeaderSelectorExample.java
@@ -0,0 +1,61 @@
+package zookeeper.curator;
+
+/**
+ * Created by hucj on 2016/3/14.
+ */
+
+import com.google.common.collect.Lists;
+import org.apache.curator.framework.CuratorFramework;
+import org.apache.curator.framework.CuratorFrameworkFactory;
+import org.apache.curator.retry.ExponentialBackoffRetry;
+import org.apache.curator.utils.CloseableUtils;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.List;
+
+/**
+ * leader选举
+ *
+ * @author hucj
+ */
+public class LeaderSelectorExample {
+
+    public static void main(String[] args) {
+
+        List<CuratorFramework> clients = Lists.newArrayList();
+        List<LeaderSelectorClient> examples = Lists.newArrayList();
+        try {
+            for (int i = 0; i < 10; i++) {
+                CuratorFramework client = CuratorFrameworkFactory.newClient("localhost",new ExponentialBackoffRetry(1000, Integer.MAX_VALUE));
+                LeaderSelectorClient example = new LeaderSelectorClient(client, "Client #" + i);
+                clients.add(client);
+                examples.add(example);
+
+                client.start();
+                example.start();
+            }
+
+            System.out.println("----------先观察一会选举的结果-----------");
+            Thread.sleep(10000);
+
+            System.out.println("----------关闭前5个客户端，再观察选举的结果-----------");
+            for (int i = 0; i < 5; i++) {
+                clients.get(i).close();
+            }
+
+            // 这里有个小技巧，让main程序一直监听控制台输入，异步的代码就可以一直在执行。不同于while(ture)的是，按回车或esc可退出
+            new BufferedReader(new InputStreamReader(System.in)).readLine();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            for (LeaderSelectorClient exampleClient : examples) {
+                CloseableUtils.closeQuietly(exampleClient);
+            }
+            for (CuratorFramework client : clients) {
+                CloseableUtils.closeQuietly(client);
+            }
+        }
+    }
+}
