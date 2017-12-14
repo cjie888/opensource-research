161bab411e4e224ea1e32c3466097fb0fb500cd5
cjie888 <cjie888@163.com>
2017-12-14 16:46:04 +0800

    test generator

--- src/main/java/test/ModelTestGenerator.java
+++ src/main/java/test/ModelTestGenerator.java
@@ -6,23 +6,66 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
 import java.lang.reflect.Method;
+import java.math.BigDecimal;
+import java.util.Date;
 
+/**
+ * @description 胡成杰(chengjiehu@creditease.cn)
+ * 2017/12/13 20:09
+ */
 public class ModelTestGenerator {
 
     public static void main(String[] args) throws IOException {
 
-        String packageName = "com.yirendai.dataengine.tbm.vo";
+        String packageName = "com.yirendai.dataengine.tbm.model";
 
-        File rootFile = new File("D:\\workspace\\yirendai-dataengine\\dataengine-tbm-module\\src\\main\\java\\com\\yirendai\\dataengine\\tbm\\vo");
+        File rootFile = new File("D:\\workspace\\yirendai-dataengine\\dataengine-tbm-module\\src\\main\\java\\com\\yirendai\\dataengine\\tbm\\model");
         for (File file : rootFile.listFiles()) {
 
 
             String className = file.getName().substring(0, file.getName().lastIndexOf("."));
             String classAllName = packageName + "." + className;
             if (!className.endsWith("Example")) {
+                continue;
+//                try {
+//                    Class clazz = Class.forName(classAllName);
+//                    Method[] methods = clazz.getDeclaredMethods();
+//                    BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(className+"Test.java")));
+//
+//                    bufferedWriter.write("package " +  packageName + ";");
+//                    bufferedWriter.newLine();
+//                    bufferedWriter.newLine();
+//                    bufferedWriter.write("import " +  classAllName + ";");
+//                    bufferedWriter.newLine();
+//                    bufferedWriter.write("import static org.junit.Assert.assertEquals;");
+//                    bufferedWriter.newLine();
+//                    bufferedWriter.write("import static org.junit.Assert.assertNotNull;");
+//                    bufferedWriter.newLine();
+//                    bufferedWriter.write("import org.junit.Test;");
+//                    bufferedWriter.newLine();
+//                    bufferedWriter.newLine();
+//                    bufferedWriter.write("public class " +  className+"Test" + " {");
+//                    bufferedWriter.newLine();
+//
+//                    for (Method method : methods) {
+//                        if (method.getName().startsWith("get")) {
+//                            generateGetMethodTest(method,bufferedWriter, className,classAllName);
+//                        } else if (method.getName().startsWith("toString")) {
+//                            generateToStringMethodTest(method,bufferedWriter, className,classAllName);
+//                        }
+//                        bufferedWriter.newLine();
+//                    }
+//                    bufferedWriter.write("}");
+//                    bufferedWriter.flush();
+//                    bufferedWriter.close();
+//                } catch (ClassNotFoundException e) {
+//                    e.printStackTrace();
+//                }
+//                System.out.println(className);
+            } else {
 
                 try {
-                    Class clazz = Class.forName(classAllName);
+                    Class clazz = Class.forName(classAllName.substring(0,classAllName.length()-7));
                     Method[] methods = clazz.getDeclaredMethods();
                     BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(className+"Test.java")));
 
@@ -30,6 +73,10 @@ public class ModelTestGenerator {
                     bufferedWriter.newLine();
                     bufferedWriter.newLine();
                     bufferedWriter.write("import " +  classAllName + ";");
+                    bufferedWriter.newLine();
+                    bufferedWriter.write(" import java.util.Date;");
+                    bufferedWriter.newLine();
+                    bufferedWriter.write(" import java.math.BigDecimal;");
                     bufferedWriter.newLine();
                     bufferedWriter.write("import static org.junit.Assert.assertEquals;");
                     bufferedWriter.newLine();
@@ -43,9 +90,7 @@ public class ModelTestGenerator {
 
                     for (Method method : methods) {
                         if (method.getName().startsWith("get")) {
-                            generateGetMethodTest(method,bufferedWriter, className,classAllName);
-                        } else if (method.getName().startsWith("toString")) {
-                            generateToStringMethodTest(method, bufferedWriter, className, classAllName);
+                            generateGetExampleMethodTest(method, bufferedWriter, className, classAllName);
                         }
                         bufferedWriter.newLine();
                     }
@@ -61,7 +106,42 @@ public class ModelTestGenerator {
 
     }
 
-        private static void generateGetMethodTest(Method method, BufferedWriter bufferedWriter, String className, String classAllName) throws IOException {
+    private static void generateGetExampleMethodTest(Method method, BufferedWriter bufferedWriter, String className, String classAllName) throws IOException {
+        String methodSuffix = method.getName().substring(3);
+        bufferedWriter.write("    @Test");
+        bufferedWriter.newLine();
+        bufferedWriter.write("    public void testGet" + methodSuffix + "() {");
+        bufferedWriter.newLine();
+        String instanceName = className.substring(0,1).toLowerCase() +  className.substring(1);
+        bufferedWriter.write("        " + className + " " + instanceName + " = new " + className  + "();");
+        bufferedWriter.newLine();
+        bufferedWriter.write("        " + className + ".Criteria criteria = " + instanceName + ".createCriteria();");
+        bufferedWriter.newLine();
+        bufferedWriter.write("        criteria.and" + methodSuffix + "IsNull();");
+        bufferedWriter.newLine();
+        bufferedWriter.write("        criteria.and" + methodSuffix + "IsNotNull();");
+        bufferedWriter.newLine();
+        bufferedWriter.write("        criteria.and" + methodSuffix + "EqualTo(" + getObject(method) + ");");
+        bufferedWriter.newLine();
+        bufferedWriter.write("        criteria.and" + methodSuffix + "NotEqualTo(" + getObject(method) + ");");
+        bufferedWriter.newLine();
+        bufferedWriter.write("        criteria.and" + methodSuffix + "GreaterThan(" + getObject(method) + ");");
+        bufferedWriter.newLine();
+        bufferedWriter.write("        criteria.and" + methodSuffix + "GreaterThanOrEqualTo(" + getObject(method) + ");");
+        bufferedWriter.newLine();
+        bufferedWriter.write("        criteria.and" + methodSuffix + "LessThan(" + getObject(method) + ");");
+        bufferedWriter.newLine();
+        bufferedWriter.write("        criteria.and" + methodSuffix + "LessThanOrEqualTo(" + getObject(method) + ");");
+        bufferedWriter.newLine();
+        bufferedWriter.write("        criteria.and" + methodSuffix + "Between(" + getObject(method)+"," + getObject(method) + ");");
+        bufferedWriter.newLine();
+        bufferedWriter.write("        criteria.and" + methodSuffix + "NotBetween(" + getObject(method)+"," + getObject(method) + ");");
+        bufferedWriter.newLine();
+        bufferedWriter.write("    }");
+        bufferedWriter.newLine();
+    }
+
+    private static void generateGetMethodTest(Method method, BufferedWriter bufferedWriter, String className, String classAllName) throws IOException {
         String methodSuffix = method.getName().substring(3);
         bufferedWriter.write("    @Test");
         bufferedWriter.newLine();
@@ -108,6 +188,15 @@ public class ModelTestGenerator {
         }
         if (returnType.isAssignableFrom(String.class)) {
             return "\"abc\"";
+        }
+        if (returnType.isAssignableFrom(Date.class)) {
+            return "new Date()";
+        }
+        if (returnType.isAssignableFrom(BigDecimal.class)) {
+            return "BigDecimal.ONE";
+        }
+        if (returnType.isAssignableFrom(Short.class)) {
+            return "Short.MIN_VALUE";
         }
         return null;
     }
