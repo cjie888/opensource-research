e63ff15d15c59d24eb28af5892b571a60e449d75
chengjie.hu@okcoin.com <chengjie.hu@okcoin.com>
2019-09-02 10:31:08 +0800

    antlr starter

---
+++ src/main/java/antlr/starter/Test.java
@@ -0,0 +1,24 @@
+package antlr.starter;
+
+import org.antlr.v4.runtime.ANTLRInputStream;
+import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.tree.ParseTree;
+import org.antlr.v4.runtime.tree.ParseTreeWalker;
+
+public class Test {
+    public static void main(String[] args) throws Exception {
+        // create a CharStream that reads from standard input
+        ANTLRInputStream input = new ANTLRInputStream("{1,2,4}"); // create a lexer that feeds off of input CharStream
+        ArrayInitLexer lexer = new ArrayInitLexer(input); // create a buffer of tokens pulled from the lexer
+        CommonTokenStream tokens = new CommonTokenStream(lexer); // create a parser that feeds off the tokens buffer
+        ArrayInitParser parser = new ArrayInitParser(tokens);
+        ParseTree tree = parser.init(); // begin parsing at init rule
+        System.out.println(tree.toStringTree(parser)); // print LISP-style tree }
+
+
+        ParseTreeWalker walker = new ParseTreeWalker();
+       // Walk the tree created during the parse, trigger callbacks
+        walker.walk(new ShortToUnicodeString(), tree);
+        System.out.println(); // print a \n after translation
+    }
+}
