d4ca887a2fca443c5454ac216d8021ae04ba2768
chengjie.hu@okcoin.com <chengjie.hu@okcoin.com>
2020-07-07 11:27:54 +0800

    json parer

--- src/main/java/antlr/starter/Test.java
+++ src/main/java/antlr/starter/Test.java
@@ -1,6 +1,7 @@
 package antlr.starter;
 
 import org.antlr.v4.runtime.ANTLRInputStream;
+import org.antlr.v4.runtime.CharStream;
 import org.antlr.v4.runtime.CommonTokenStream;
 import org.antlr.v4.runtime.tree.ParseTree;
 import org.antlr.v4.runtime.tree.ParseTreeWalker;
@@ -8,7 +9,7 @@ import org.antlr.v4.runtime.tree.ParseTreeWalker;
 public class Test {
     public static void main(String[] args) throws Exception {
         // create a CharStream that reads from standard input
-        ANTLRInputStream input = new ANTLRInputStream("{1,2,4}"); // create a lexer that feeds off of input CharStream
+        CharStream input = new ANTLRInputStream("{1,2,4}"); // create a lexer that feeds off of input CharStream
         ArrayInitLexer lexer = new ArrayInitLexer(input); // create a buffer of tokens pulled from the lexer
         CommonTokenStream tokens = new CommonTokenStream(lexer); // create a parser that feeds off the tokens buffer
         ArrayInitParser parser = new ArrayInitParser(tokens);
